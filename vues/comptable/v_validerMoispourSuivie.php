d0b0faee9cd5f2de3fbc18999bafa412c73cd1fe
root <root@chris-desktop>
2017-02-20 00:08:24 +0100

    Fin de la mission 1

--- vues/comptable/v_validerMoispourSuivie.php
+++ vues/comptable/v_validerMoispourSuivie.php
@@ -8,66 +8,59 @@
                 <select id="choiMois" name="choiMois">
 
                     <?php
-                    //Initialise les variables tmpMois, tmpMoisDouble pour avoir le 1er mois des fiches de frais en etat Validee
+                    //Initialise les variables tmpMois et visiteuraValider pour avoir les mois et les Visiteurs des fiches de frais en etat Validee
+                    $visiteursaValider = array();
+                    $tmpMois = array();
                     foreach ($lesFichesFrais as $uneFicheFrais) {
-                        if ($uneFicheFrais['libelle'] == 'Validée et mise en paiement') {
-                            $tmpMoisDouble = array($uneFicheFrais['mois']);
-                            $tmpMois = array($uneFicheFrais['mois']);
-
-                            $mois = $uneFicheFrais['mois'];
-                            $numAnnee = substr($uneFicheFrais['mois'], 0, 4);
-                            $numMois = substr($uneFicheFrais['mois'], 4, 6);
-                            ?>
-                            <option value = "<?php echo $mois ?>"><?php echo $numMois . "/" . $numAnnee ?></option>
-                            <?php
-                            break;
-                        }
-                    }
-
-                    //Recupere tout les mois avec une fiche de frais en etat Validee
-                    foreach ($lesFichesFrais as $uneFicheFrais) {
-
                         if ($uneFicheFrais['libelle'] == 'Validée et mise en paiement') {
                             array_push($tmpMois, $uneFicheFrais['mois']);
-                            $diff = array_diff($tmpMois, $tmpMoisDouble);
-                            if ($diff != null) {
-                                array_push($tmpMoisDouble, $uneFicheFrais['mois']);
-
-                                $mois = $uneFicheFrais['mois'];
-                                $numAnnee = substr($uneFicheFrais['mois'], 0, 4);
-                                $numMois = substr($uneFicheFrais['mois'], 4, 6);
-                                if ($mois == $moisSelectionner) {
-                                    ?>
-                                    <option selected value="<?php echo $mois ?>"><?php echo $numMois . "/" . $numAnnee ?> </option>
-                                    <?php
-                                } else {
-                                    ?>
-                                    <option value="<?php echo $mois ?>"><?php echo $numMois . "/" . $numAnnee ?> </option>
-                                    <?php
-                                }
-                            }
+                            array_push($visiteursaValider, $uneFicheFrais['idVisiteur']);
                         }
                     }
+
+                    $tmpMois = array_unique($tmpMois);
+                    //Recupere tous les mois avec une fiche de frais en etat Validee
+                    foreach ($tmpMois as $unMois) {
+                        $mois = $unMois;
+                        $numAnnee = substr($mois, 0, 4);
+                        $numMois = substr($mois, 4, 6);
+                        if ($mois == $moisSelectionner) {
+                            ?>
+                            <option selected value="<?php echo $mois ?>"><?php echo $numMois . "/" . $numAnnee ?> </option>
+                            <?php
+                        } else {
+                            ?>
+                            <option value="<?php echo $mois ?>"><?php echo $numMois . "/" . $numAnnee ?> </option>
+                            <?php
+                        }
+                    }
+
+
+                    $visiteursaValider = array_unique($visiteursaValider);
                     ?>    
 
 
                 </select>
             </p>
-            <p> <!-- choix du visiteur avec une liste déroulante nom et prénom -->
+            <p> 
                 <label for="choiVisiteur">Choisir Visiteur : </label>
                 <select id="choiVisiteur" name="choiVisiteur">
                     <?php
                     foreach ($lesVisiteurs as $unvisiteur) {
                         if ($unvisiteur['fonction'] == 1) {
-                            $VisiteurSelectionner = str_replace(' ', '', $VisiteurSelectionner);
-                            if ($unvisiteur['id'] == $VisiteurSelectionner) {
-                                ?>
-                                <option selected value="<?php echo $unvisiteur['id'] ?>"   ><?php echo $unvisiteur['nom'] . " " . $unvisiteur['prenom'] ?></option> 
-                                <?php
-                            } else {
-                                ?>
-                                <option value="<?php echo $unvisiteur['id'] ?>" ><?php echo $unvisiteur['nom'] . " " . $unvisiteur['prenom'] ?></option>
-                                <?php
+                            foreach ($visiteursaValider as $visiteurFiche) {
+                                if ($unvisiteur['id'] == $visiteurFiche) {
+                                    $VisiteurSelectionner = str_replace(' ', '', $VisiteurSelectionner);
+                                    if ($unvisiteur['id'] == $VisiteurSelectionner) {
+                                        ?>
+                                        <option selected value="<?php echo $unvisiteur['id'] ?>"   ><?php echo $unvisiteur['nom'] . " " . $unvisiteur['prenom'] ?></option> 
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <option value="<?php echo $unvisiteur['id'] ?>" ><?php echo $unvisiteur['nom'] . " " . $unvisiteur['prenom'] ?></option>
+                                        <?php
+                                    }
+                                }
                             }
                         }
                     }
